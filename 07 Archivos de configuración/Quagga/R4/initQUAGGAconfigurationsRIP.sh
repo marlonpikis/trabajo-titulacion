23593fcf3db9ee3628972098ea5359d2610426b4
marlonpikis <marlonpikis@outlook.com>
2024-06-23 19:50:42 -0500

    Add files via upload

---
+++ 07 Archivos de configuración/Quagga/R4/initQUAGGAconfigurationsRIP.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+<< COMMENT 
+Universidad de Cuenca
+Facultad de Ingeniería
+Carrera de Telecomunicaciones
+
+Trabajo de titulación
+
+Autores:
+	Marlon Xavier Gomezcoello Rodríguez
+	Justin Mateo Picón Barros
+
+Inicializar el servicio de QUAGGA RIP en R4
+COMMENT
+
+sudo ovs-vsctl --if-exists del-br br0
+sudo systemctl stop ovs-vswitchd.service
+
+sudo systemctl start zebra
+sudo systemctl start ripd
